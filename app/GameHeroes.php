de6c4b46fa32f5b2c807f81f6212067651acbfe6
Gabriel 'Hazriel' Bertini <gabriel.bertini@epita.fr>
2017-09-21 20:52:10 +0200

    [HEROES] Added heroes display

--- app/GameHeroes.php
+++ app/GameHeroes.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class GameHeroes extends Model
 {
@@ -21,6 +22,52 @@ class GameHeroes extends Model
     public function stats()
     {
         return $this->hasMany('App\GameStats', 'heroID');
+    }
+
+    public function getTeam()
+    {
+        $team = 0;
+        foreach ($this->stats as $stat)
+        {
+            if ($stat->statsKey == "c_team")
+                $team = $stat->statsValue;
+        }
+        switch ($team)
+        {
+            case 1:
+                return "national";
+            case 2:
+                return "royal";
+            default:
+                return "error_team";
+        }
+    }
+
+    public function getClass()
+    {
+        $class = 0;
+        foreach ($this->stats as $stat)
+        {
+            if ($stat->statsKey == "c_kit") {
+                $class = $stat->statsValue;
+            }
+        }
+        switch ($class)
+        {
+            case "0":
+                return "commando";
+            case "1":
+                return "soldier";
+            case "2":
+                return "gunner";
+            default:
+                return "error_class";
+        }
+    }
+
+    public function getStat($stat)
+    {
+        return GameStats::where('heroID', $this->id)->where('statsKey', $stat)->first()->statsValue;
     }
 
     public $timestamps = false;
--- resources/views/heroes/view.blade.php
+++ resources/views/heroes/view.blade.php
@@ -10,11 +10,31 @@
                 <div class="nbd-section-body">
                     <p><a href="{{ route('heroes.createForm') }}"><button type="button" class="btn btn-success"><span class="glyphicon glyphicon-plus-sign"></span> Create a Hero</button></a></p>
                     <p>Token : {{ Auth::user()->game_token }}</p>
-					<ul>
-						@foreach(Auth::user()->heroes as $hero)
-						<li>{{ $hero->heroName }}</li>
-						@endforeach
-					</ul>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <ul>
+                                @foreach(Auth::user()->heroes as $hero)
+                                    <div class="col-lg-10 hero">
+                                        <div class="hero-image image-{{ $hero->getTeam() }}">
+                                        <span class="image-{{ $hero->getClass() }}"
+                                              style="background-image: url({{ asset('/images/herocreator/icon_'.$hero->getTeam().'_classes.png') }});">
+                                        </span>
+                                        </div>
+                                        <div class="hero-content">
+                                            <span class="name">{{ $hero->heroName }}</span>
+                                            <div class="info">
+                                                <div class="info-content col-lg-11">
+                                                    <div class="col-lg-4 col-xs-4 hero-team team-{{ $hero->getTeam() }}"><span class="hero-info">{{ $hero->getTeam() }}</span></div>
+                                                    <div class="col-lg-4 col-xs-4 hero-class class-{{ $hero->getClass() }}"><span class="hero-info">{{ $hero->getClass() }}</span></div>
+                                                    <div class="col-lg-4 col-xs-4 hero-level level-{{ (int) $hero->getStat('level') }}"><span class="hero-info">level</span></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
